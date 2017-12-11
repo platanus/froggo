a49d234b419e0b377d62dc7b1ba687a53343f89c
Fernanda <fernanda@platan.us>
2017-12-11 16:03:27 -0300

    feat(admin): allow track/untrack repositories

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,10 +2,28 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
+
+    columns do
+      column do
+        panel "Tracked repositories" do
+          table_for Repository.where("tracked = true").map do
+            column :full_name
+            column :tracked
+            column "Edit" do |repo| # TO DO: convert to checkbox
+              link_to "Untrack", edit_admin_repository_url(repo)
+            end
+            column "See open pull-requests" do # TO DO: show PR by repo
+              link_to "Pull requests", nil
+            end
+          end
+          small link_to("Manage repositories tracked here.", "repositories")
+        end
       end
     end
 
