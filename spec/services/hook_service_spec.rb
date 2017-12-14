effc8c63a0fee116109d3913b6d3097450f9e7c2
Fernanda <fernanda@platan.us>
2017-12-14 15:58:51 -0300

    test(): services structure

--- spec/services/hook_service_spec.rb
+++ spec/services/hook_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe HookService do
   let (:admin_user) { double }
   let (:repository) { double(id: 5) }
-  let (:hook) { create(:hook, id: 5) }
+  let (:hook) { create(:hook) }
 
   def build
     described_class.new(user: admin_user)
@@ -14,7 +14,40 @@ describe HookService do
   end
 
   context "suscribe" do
-    it "suscribe a hook" do
-    end
+    # it "to an existent hook should call edit" do
+    #   hook.repository_id = 5
+    #   expect(HookService).to receive(:subscribe).with(repository)
+    # end
+
+    # it "to a new hook should call create" do
+    #   hook.id = 9
+    #   expect(HookService).to receive(:subscribe).with(hook)
+    # end
+  end
+
+  context "unsubscribe" do
+    # it "to an existen hooks should call edit"
+    # end
+
+    # it "to an inexistent hook should fail"
+    # end
+  end
+
+  context "create_new_hook" do
+    # it "should make a request to octokit create_hook" do
+    # end
+  end
+
+  context "edit_active_hook" do
+    # it "should change hooks status" do
+    # end
+
+    # it "should make a request to octokit edit_hook" do
+    # end
+  end
+
+  context "create" do
+    # it "should create a hook"
+    # end
   end
 end
