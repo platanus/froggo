7f9e8e2d57130df712b46874e330dcc679e992d9
Óscar Estay <oaestay@uc.cl>
2017-12-14 12:35:33 -0300

    feat(AdminUserObserver): Add AdminUserObserver to fetch orgs

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,5 @@
 class AdminUser < ApplicationRecord
+  include PowerTypes::Observable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
