3668478ea862ca4a959db20b98d83ccaee0969d1
Óscar Estay <oaestay@uc.cl>
2017-12-14 11:42:10 -0300

    feat(Organization): Register Organization in ActiveAdmin

---
+++ app/admin/organization.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register Organization do
+  remove_filter :gh_id, :id
+  permit_params :name, :tracked
+  menu priority: 1
+  actions :all
+
+  index do
+    selectable_column
+    column :login
+    column :name
+    column :description
+    toggle_bool_column :tracked
+    column :html_url do |o|
+      link_to "Link", o.html_url
+    end
+    column :created_at
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :tracked
+    end
+    f.actions
+  end
+end
