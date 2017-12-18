94e7e8c3083cdbbe93b4fc84664494841db69eb4
Fernanda <fernanda@platan.us>
2017-12-18 20:35:18 -0300

    test(webhook): test post route for github

--- spec/controllers/webhook_controller_spec.rb
+++ spec/controllers/webhook_controller_spec.rb
@@ -10,8 +10,9 @@ RSpec.describe WebhookController, type: :controller do
 
   describe "POST #receive" do
     # it "returns json success" do
+    #   @request.headers['X-Hub-Signature'] = 'sha1=fbdb1d1b18aa6c08324b7d64b71fb76370690e1d'
     #   post :receive
-    #   expect(response).to have_http_status(401)
+    #   expect(response).to have_http_status(200)
     # end
   end
 end
