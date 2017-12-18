8136bc2730798c56f8a34e600ae708d02fc5c2b5
Fernanda <fernanda@platan.us>
2017-12-18 20:34:23 -0300

    feat(webhook): fix github sha1 signature

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -2,14 +2,21 @@ class WebhookController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    render json: { response: JSON.parse($redis.get('last event')), status: 200 }, status: 200
+    render json: { response: JSON($redis.get('last event')), status: 200 }, status: 200
   end
 
   def receive
-    $redis.set('last event', request.body.to_json)
-    if request["X-Hub-Signature"] == ENV["GH_HOOK_SECRET"]
+    $redis.set('last event', JSON(request.body))
+    if verify_signature(request)
       render json: { response: "ok", status: 200 }, status: 200
+    else
+      render json: { error: "not auth", status: 401 }, status: 401
     end
-    render json: { error: "not auth", status: 401 }, status: 401
+  end
+
+  def verify_signature(request)
+    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'),
+      ENV['GH_HOOK_SECRET'], request.body.read)
+    Rack::Utils.secure_compare(signature, request.headers['X-Hub-Signature'])
   end
 end
