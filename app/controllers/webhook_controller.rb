826d371b64fb4db53c14018e25fdef8105541ed7
Fernanda <fernanda@platan.us>
2017-12-19 11:25:50 -0300

    chore(): example of payload attributes

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -2,20 +2,45 @@ class WebhookController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
+    @event = JSON.parse($redis.get('last event'))
+    manage_request(@event) # <- comentar esta cuando este listo
     render json: {
-      active: $redis.get('active'),
-      response: JSON.parse($redis.get('last event')),
+      action: @event['action'],
+      user: $redis.get('user'),
+      response: @event,
       status: 200
     }, status: 200
   end
 
   def receive
-    $redis.set('last event', request.body.read)
-    $redis.set('active', request.body.read["active"])
+    @event = request.body.read
     if verify_signature(request)
+      # manage_request(JSON.parse(@event)) # <- Giovanni aquí llamar
+      $redis.set('last event', @event)
       render json: { response: "ok", status: 200 }, status: 200
     else
       render json: { error: "not auth", status: 401 }, status: 401
+    end
+  end
+
+  def manage_request(request)
+    if request['action'] == 'review_requested'
+      p request['pull_request']['id']
+      p request['pull_request']['state']
+      p request['pull_request']['user']['login']
+      p request['pull_request']['user']['id']
+      p request['pull_request']['user']['avatar_url']
+      p request['pull_request']['user']['html_url']
+      request['pull_request']['requested_reviewers'].each do |reviewer|
+        p reviewer['login']
+        p reviewer['id']
+        p reviewer['avatar_url']
+        p reviewer['html_url']
+      end
+      p request['pull_request']['head']['repo']['id']
+      p request['pull_request']['head']['repo']['full_name']
+      p request['pull_request']['head']['repo']['owner']['login']
+      p request['pull_request']['head']['repo']['owner']['id']
     end
   end
 
