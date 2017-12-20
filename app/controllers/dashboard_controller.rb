dc0d243997a3a6fca47021a0d412f95e9bab12c6
Fernanda <fernanda@platan.us>
2017-12-20 19:30:24 -0300

    feat(dashboard): avoid show table if not data

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,4 @@ class DashboardController < ApplicationController
     @corrmat = CorrelationMatrix.new(GithubUser.where(tracked: true))
     @corrmat.fill_matrix(PullRequest.where(owner_id: GithubUser.where(tracked: true).pluck(:id)))
   end
-
-  def get_owners_column(users)
-    PullRequest.where(owner_id: users.pluck(:id))
-  end
 end
