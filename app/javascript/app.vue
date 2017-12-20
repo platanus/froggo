2a8cb5b58f4ac85a0d1bd7687e2eaae9d423185d
Fernanda <fernanda@platan.us>
2017-12-20 18:56:34 -0300

    chore(): comment vue examples

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <!-- <p>{{ message }}</p> -->
   </div>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
-  }
-}
+// export default {
+//   data: function () {
+//     return {
+//       message: "Hello Vue!"
+//     }
+//   }
+// }
 </script>
 
 <style scoped>
-p {
+/* p {
   font-size: 2em;
   text-align: center;
-}
+} */
 </style>
