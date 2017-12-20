a3230788a0a128edbc67d2091c41e46ceeb34b7a
oaestay <oaestay@uc.cl>
2017-12-20 18:35:36 -0300

    feat(OrganizationObserver): Add OrganizationObserver

--- app/observers/organization_observer.rb
+++ app/observers/organization_observer.rb
@@ -3,7 +3,10 @@ class OrganizationObserver < PowerTypes::Observer
 
   def create_organization_repositories
     if object.tracked
+      HookService.new.subscribe(object)
       GithubWorker.perform_async("FETCH_ORG_REPOS", organization_id: object.id)
+    elsif !object.id_changed?
+      HookService.new.unsubscribe(object)
     end
   end
 end
