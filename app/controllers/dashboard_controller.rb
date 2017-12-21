9937fcc63b5e1723006a695a781fa0d644097177
María Fernanda <mfsepulveda@uc.cl>
2017-12-21 18:17:46 -0300

    Revert "Dashboard"

--- app/controllers/dashboard_controller.rb
+++
@@ -1,6 +0,0 @@
-class DashboardController < ApplicationController
-  def index
-    @corrmat = CorrelationMatrix.new(GithubUser.where(tracked: true))
-    @corrmat.fill_matrix(PullRequest.where(owner_id: GithubUser.where(tracked: true).pluck(:id)))
-  end
-end
