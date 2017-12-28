1df4ea5b9372a82b906d34e48c616cbf5940a3af
Giovanni De Lucca <giovanni@platan.us>
2017-12-28 14:57:46 -0300

    feat(admin): add repository and owner column to pull requests

--- app/admin/pull_request.rb
+++ app/admin/pull_request.rb
@@ -3,11 +3,19 @@ ActiveAdmin.register PullRequest do
   belongs_to :repository, optional: true
   menu priority: 5
 
+  preserve_default_filters!
+  filter :owner, collection: -> { GithubUser.all.pluck(:login, :id) }
+
   index do
     column :id
     column :gh_id
     column :title
     column :gh_number
+    column :repository
+    column :owner do |pr|
+      link_to pr.owner.login,
+              admin_github_user_path(pr.owner_id)
+    end
     column :pr_state
     column :html_url
     column :relations do |pr|
@@ -15,7 +23,6 @@ ActiveAdmin.register PullRequest do
         admin_pull_request_pull_request_relations_path(pr)
     end
     column :gh_created_at
-    column :gh_merged_at
 
     actions
   end
