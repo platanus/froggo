e14cf3c08c1808ffb8a7ac88f0eea0c23aeec071
Fernanda <fernanda@platan.us>
2018-01-18 21:28:17 -0300

    feat(): add config panel vue component

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,18 @@
 /* eslint no-console: 0 */
 
 import Vue from 'vue';
-import App from '../app.vue';
+import Selector from '../app.vue';
+import ConfigPanel from '../panel.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('selector').appendChild(document.createElement('selector'));
-  const app = new Vue({
-    render: h => h(App)
+  const selector = new Vue({
+    render: h => h(Selector)
   }).$mount('selector');
+
+  document.getElementById('panel').appendChild(document.createElement('panel'));
+  const panel = new Vue({
+    render: h => h(ConfigPanel)
+  }).$mount('panel');
 })
-
-
-
 
