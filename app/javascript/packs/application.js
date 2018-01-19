98f36caf1889fea62ad617683a2503f9389343df
Fernanda <fernanda@platan.us>
2018-01-18 21:29:17 -0300

    feat(): configure component in index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,13 +6,13 @@ import ConfigPanel from '../panel.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('selector').appendChild(document.createElement('selector'));
-  const selector = new Vue({
-    render: h => h(Selector)
+  new Vue({
+    render: h => h(Selector),
   }).$mount('selector');
 
   document.getElementById('panel').appendChild(document.createElement('panel'));
-  const panel = new Vue({
-    render: h => h(ConfigPanel)
+  new Vue({
+    render: h => h(ConfigPanel),
   }).$mount('panel');
-})
+});
 
