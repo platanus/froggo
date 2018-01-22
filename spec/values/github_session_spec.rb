80c48d063ebbf9014ac9d39eb65de2e528aec82e
Fernanda <fernanda@platan.us>
2018-01-22 19:09:21 -0300

    feat(GithubSession): add value for github user session

---
+++ spec/values/github_session_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe CorrelationMatrix, type: :class do
+  # context 'when initialized with correct values' do
+  #   subject { GithubSession.new('a name', 'a token', '','admin') }
+  #   it 'has empty data' do
+  #     expect(subject.data).to be_empty
+  #   end
+  # end
+end
