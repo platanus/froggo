017a820d5e5a318e35c45b3ef58cb0c8c74eb912
Jaime Bünzli <jpbunzli@gmail.com>
2018-01-23 18:20:32 -0300

    feat(github_pull_request_service): initialize with token

--- app/services/github_pull_request_service.rb
+++ app/services/github_pull_request_service.rb
@@ -1,9 +1,15 @@
-class GithubPullRequestService < PowerTypes::Service.new
+class GithubPullRequestService < PowerTypes::Service.new(:token)
   def import_all_from_repository(repository)
     # Obtain list of pull request from repository and persist them in DB.
   end
 
   def handle_webhook_event(data)
     # Receive a pull request event (CRUD) and process it.
+  end
+
+  private
+
+  def client
+    @client ||= BuildOctokitClient.for(token: @token)
   end
 end
