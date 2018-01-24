ce1d2f3eae9e8d73c291d7d743c7584dc23e9eeb
Jaime Bünzli <jpbunzli@gmail.com>
2018-01-24 14:57:01 -0300

    feat(repository-tracking-service): new track method The repository clears its PRs and Reviews and trigger the historic import from github

--- app/services/repository_tracking_service.rb
+++ app/services/repository_tracking_service.rb
@@ -1,2 +1,24 @@
 class RepositoryTrackingService < PowerTypes::Service.new(:repository, :token)
+  def track
+    clear_repository
+    import_pull_requests_with_reviews
+    set_repository_status(true)
+  end
+
+  private
+
+  def clear_repository
+    @repository.pull_requests.destroy_all
+    @repository.pull_request_reviews.destroy_all
+  end
+
+  def import_pull_requests_with_reviews
+    GithubPullRequestService.new(token: @token).import_all_from_repository(@repository)
+    GithubPullRequestReviewService.new(token: @token).import_all_from_repository(@repository)
+  end
+
+  def set_repository_status(tracked)
+    @repository.tracked = tracked
+    @repository.save
+  end
 end
--- spec/services/repository_tracking_service_spec.rb
+++ spec/services/repository_tracking_service_spec.rb
@@ -1,9 +1,44 @@
 require 'rails_helper'
 
 describe RepositoryTrackingService do
+  let(:token) { "blubli" }
+  let(:repository) { create(:repository) }
+
+  let(:service) { build(repository: repository, token: token) }
+
   def build(*_args)
     described_class.new(*_args)
   end
 
-  pending "describe what your service does here"
+  before do
+    expect_any_instance_of(GithubPullRequestService).to receive(:import_all_from_repository)
+      .and_return(true)
+
+    expect_any_instance_of(GithubPullRequestReviewService).to receive(:import_all_from_repository)
+      .and_return(true)
+  end
+
+  describe "#track" do
+    context "when PR exists and no PR where imported" do
+      before do
+        pullrequest1 = create(:pull_request, repository: repository)
+        pullrequest2 = create(:pull_request, repository: repository)
+        create(:pull_request_review, pull_request: pullrequest1)
+        create(:pull_request_review, pull_request: pullrequest1)
+        create(:pull_request_review, pull_request: pullrequest2)
+      end
+
+      it "ends with no pull requests" do
+        expect { service.track }.to change { PullRequest.count }.from(2).to(0)
+      end
+
+      it "ends with no pull request reviews" do
+        expect { service.track }.to change { PullRequestReview.count }.from(3).to(0)
+      end
+    end
+
+    it "change repository tracked status" do
+      expect { service.track }.to change { repository.tracked }.from(false).to(true)
+    end
+  end
 end
