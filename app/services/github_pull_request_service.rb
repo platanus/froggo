b50314f3a199e35f642553f4495b690ebaf20f05
Jaime Bünzli <jpbunzli@gmail.com>
2018-01-23 23:13:34 -0300

    feat(gh_services): import all github PR from a repository

--- app/services/github_pull_request_service.rb
+++ app/services/github_pull_request_service.rb
@@ -1,6 +1,8 @@
 class GithubPullRequestService < PowerTypes::Service.new(:token)
   def import_all_from_repository(repository)
-    # Obtain list of pull request from repository and persist them in DB.
+    github_pull_requests(repository).each do |github_pull_request|
+      import_github_pull_request(repository, github_pull_request)
+    end
   end
 
   def handle_webhook_event(data)
@@ -8,6 +10,32 @@ class GithubPullRequestService < PowerTypes::Service.new(:token)
   end
 
   private
+
+  def github_pull_requests(repository)
+    client.pull_requests(repository.full_name, state: 'all')
+  end
+
+  def import_github_pull_request(repository, github_pull_request)
+    owner = GithubUserService.new.find_or_create(github_pull_request.user)
+    pull_request_params = get_pull_request_params(github_pull_request).merge(owner_id: owner.id)
+
+    repository.pull_requests
+              .create_with(pull_request_params)
+              .find_or_create_by!(gh_id: github_pull_request.id)
+  end
+
+  def get_pull_request_params(github_pull_request)
+    {
+      pr_state: github_pull_request.state,
+      title: github_pull_request.title,
+      gh_number: github_pull_request.number,
+      html_url: github_pull_request.html_url,
+      gh_created_at: github_pull_request.created_at,
+      gh_updated_at: github_pull_request.updated_at,
+      gh_closed_at: github_pull_request.closed_at,
+      gh_merged_at: github_pull_request.merged_at
+    }
+  end
 
   def client
     @client ||= BuildOctokitClient.for(token: @token)
--- spec/services/github_pull_request_service_spec.rb
+++ spec/services/github_pull_request_service_spec.rb
@@ -1,9 +1,79 @@
 require 'rails_helper'
 
 describe GithubPullRequestService do
+  let(:token) { "blubli" }
+  let(:repository) { create(:repository) }
+
+  let(:service) { build(token: token) }
+
+  let(:github_prs_response) do
+    [
+      double(
+        id: 3,
+        title: "Test",
+        number: 1,
+        state: "open",
+        html_url: "http://www.gihub.com/prueba",
+        created_at: "2017-12-12 09:17:52",
+        updated_at: "2017-12-12 09:17:52",
+        closed_at: "2017-12-12 09:17:52",
+        merged_at: nil,
+        user: double(
+          id: 1,
+          login: "milla",
+          name: "Milla Jovovich",
+          email: "milla@jovovich.cl",
+          avatar_url: "https://avatars2.githubusercontent.com/u/741483?v=4",
+          html_url: "https://github.com/bunzli"
+        )
+      )
+    ]
+  end
+
+  let(:client) { double(:client, pull_requests: true) }
+
   def build(*_args)
     described_class.new(*_args)
   end
 
-  pending "describe what your service does here"
+  before do
+    allow(BuildOctokitClient).to receive(:for).with(token: token).and_return(client)
+
+    allow(client).to receive(:pull_requests).with(repository.full_name, state: "all")
+                                            .and_return(github_prs_response)
+  end
+
+  describe "#import_all_from_repository" do
+    context "when PR doesn`t exist" do
+      it "creates new pull request" do
+        expect { service.import_all_from_repository(repository) }.to(
+          change { PullRequest.count }.by(1)
+        )
+      end
+
+      it "creates PR with valid data" do
+        service.import_all_from_repository(repository)
+        pull_request = PullRequest.last
+
+        expect(pull_request).to have_attributes(
+          gh_id: 3,
+          gh_number: 1,
+          pr_state: "open",
+          title: "Test"
+        )
+      end
+    end
+
+    context "when PR exists" do
+      before do
+        create(:pull_request, gh_id: 3, repository: repository)
+      end
+
+      it "does not create new pull requests" do
+        expect { build(token: token).import_all_from_repository(repository) }.not_to(
+          change { PullRequest.count }
+        )
+      end
+    end
+  end
 end
