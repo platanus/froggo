815bfdc5290d8f5e1e9e55de4a3e0ab4d394ce4e
Jaime Bünzli <jpbunzli@gmail.com>
2018-01-23 23:20:11 -0300

    feat(github_pull_request_reviews_service): initialize with token

--- app/services/github_pull_request_review_service.rb
+++ app/services/github_pull_request_review_service.rb
@@ -1,4 +1,4 @@
-class GithubPullRequestReviewService < PowerTypes::Service.new
+class GithubPullRequestReviewService < PowerTypes::Service.new(:token)
   def import_all_from_repository(repository)
     # Obtain list of reviews for each PR of the repository and pull request
     # and persist them in DB.
@@ -6,5 +6,11 @@ class GithubPullRequestReviewService < PowerTypes::Service.new
 
   def handle_webhook_event(data)
     # Receive review event and process it.
+  end
+
+  private
+
+  def client
+    @client ||= BuildOctokitClient.for(token: @token)
   end
 end
