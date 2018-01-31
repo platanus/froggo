d51f48e0f39d63c7a16af385d7c11ca725a289cc
Fernanda <fernanda@platan.us>
2018-01-31 14:51:43 -0300

    feat(vue): change vue structure to be more explicative

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,19 +2,26 @@
 
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
-import App from '../app.vue';
+import Selector from '../selector.vue';
 import Locales from '../locales.js';
 
 Vue.use(VueI18n);
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('selector').appendChild(document.createElement('selector'));
-  const app = new Vue({
-    render: h => h(App),
+
+  let renderElement = null;
+  let idElement = '';
+
+  if (document.getElementById('selector')) {
+    renderElement = Selector;
+    idElement = '#selector';
+  }
+
+  new Vue({
+    render: h => h(renderElement),
     i18n: new VueI18n({
       locale: 'es',
       messages: Locales.messages,
     }),
-  }).$mount('selector');
+  }).$mount(idElement);
 });
-
