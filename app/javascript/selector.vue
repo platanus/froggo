2b9d3a1a3ae6352395a52e4230ba4327c334481d
Fernanda <fernanda@platan.us>
2018-01-31 16:30:53 -0300

    feat(selector): remove cookies from component

--- app/javascript/selector.vue
+++ app/javascript/selector.vue
@@ -1,57 +1,30 @@
 <template>
-<div id="selector">
+  <div id="selector">
     <select v-model="selected">
-      <option v-for="option in options" v-bind:value="option.value">
-        {{ option.text }}
+      <option v-for="option in options" :value="option" :key="option">
+        {{ option }}
       </option>
     </select>
     <button v-on:click="redirect(selected)">{{ $parent.$t("message.selector.button") }}</button>
-</div>
+  </div>
 </template>
 
 <script>
-function getCookies() {
-  const cookies_saved = document.cookie.split('; ');
-  let cookies = {};
-  for (let i = 0; i < cookies_saved.length; i++) {
-    const temp = cookies_saved[i].split('=');
-    cookies[temp[0]] = temp[1];
-  }
-  return cookies;
-}
-
-function getOrgsCookie() {
-  return JSON.parse(decodeURIComponent(getCookies()['orgs']));
-}
-
-function currentOrg() {
-  if(getCookies()['currentOrg'] === undefined) {
-    document.cookie = "currentOrg="+getOrgsCookie()[0].login;
-  }
-  return getCookies()['currentOrg'];
-}
-
 export default {
   data: function () {
     return {
-      selected: currentOrg(),
+      selected: 'example',
     }
   },
   computed: {
     options() {
-      currentOrg();
-      let cookies = getOrgsCookie();
-      let options = [];
-      for (let i = 0; i < cookies.length; i++) {
-        options.push({text: cookies[i].login, value: cookies[i].login });
-      }
+      let options = ['some', 'example', 'data'];
       return options;
     }
   },
   methods: {
     redirect(dashboard) {
-      document.cookie = "currentOrg="+dashboard;
-      document.location.href = '/dashboard/' + dashboard;
+      document.location.href = `/dashboard/${dashboard}`;
     }
   }
 }
