598570e0af00dfd6559213a4783f65af35dc5619
Jaime Bünzli <jpbunzli@gmail.com>
2018-02-02 13:11:17 -0300

    chore(github_webhook): comment the webhook router This should use the new github services

--- app/commands/process_webhook_event.rb
+++ app/commands/process_webhook_event.rb
@@ -1,11 +1,13 @@
 class ProcessWebhookEvent < PowerTypes::Command.new(request: nil, event: nil)
   def perform
-    if @event == 'pull_request'
-      PullRequestService.new(payload: @request).process
-    elsif @event == 'pull_request_review'
-      PullRequestReviewService.new(payload: @request).process
-    elsif @event == 'repository'
-      RepositoryService.new(payload: @request).process
-    end
+    # TODO: this should use the new GithubServices
+
+    # if @event == 'pull_request'
+    #   PullRequestService.new(payload: @request).process
+    # elsif @event == 'pull_request_review'
+    #   PullRequestReviewService.new(payload: @request).process
+    # elsif @event == 'repository'
+    #   RepositoryService.new(payload: @request).process
+    # end
   end
 end
