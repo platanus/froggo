0f55ce5d2e84643178eab07c6683f15614655d67
Jaime Bünzli <jpbunzli@gmail.com>
2018-02-05 18:19:05 -0300

    chore(repositories): list repositories using vue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 import Vue from 'vue/dist/vue.esm';
 import VueI18n from 'vue-i18n';
 import Selector from '../selector.vue';
+import Repository from '../repository.vue';
 import Locales from '../locales.js';
 
 Vue.use(VueI18n);
@@ -10,6 +11,7 @@ Vue.use(VueI18n);
 document.addEventListener('DOMContentLoaded', () => {
 
   Vue.component('selector', Selector);
+  Vue.component('repository', Repository);
 
   new Vue({ // eslint-disable-line no-new
     el: '#app',
---
+++ app/javascript/repository.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <div>{{ repository.name }}</div>
+</div>
+</template>
+
+<script>
+
+export default {
+  data: function () {
+    return {
+      repository: this.value,
+    }
+  },
+  props: ['value'],
+}
+</script>
