a49760efd5ed90c5cb4ff854a8282d766e9f97de
Fernanda <fernanda@platan.us>
2018-04-10 11:07:21 -0300

    fix(el-app): avoid console error in landing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 /* eslint no-console: 0 */
+/* global document, window */
 
 import Vue from 'vue/dist/vue.esm';
 import VueI18n from 'vue-i18n';
@@ -11,34 +12,36 @@ Vue.use(VueI18n);
 document.addEventListener('DOMContentLoaded', () => {
   Vue.component('repository', Repository);
   Vue.component('dropdown', Dropdown);
-
-  new Vue({ // eslint-disable-line no-new
-    el: '#app',
-    i18n: new VueI18n({
-      locale: 'es',
-      messages: Locales.messages,
-    }),
-    data: {
-      activeOrg: null,
-    },
-    methods: {
-      changeOrganization(organization) {
-        if (organization === null) {
-          return;
-        }
-        if (this.activeOrg !== organization.login) {
-          document.location.href = `/organizations/${organization.login}`;
-        }
-
-        return;
+  if (document.getElementById('app') !== null) {
+    new Vue({ // eslint-disable-line no-new
+      el: '#app',
+      i18n: new VueI18n({
+        locale: 'es',
+        messages: Locales.messages,
+      }),
+      data: {
+        activeOrg: null,
       },
-    },
-    beforeMount() {
-      const pathArray = window.location.pathname.split('/');
-      const orgNameUrlPos = 1;
-      if (pathArray.length > orgNameUrlPos + 1 && pathArray[orgNameUrlPos] === 'organizations') {
-        this.activeOrg = pathArray[orgNameUrlPos + 1];
-      }
-    },
-  });
+      methods: {
+        changeOrganization(organization) {
+          if (organization === null) {
+            return;
+          }
+          if (this.activeOrg !== organization.login) {
+            document.location.href = `/organizations/${organization.login}`;
+          }
+
+          return;
+        },
+      },
+      beforeMount() {
+        const pathArray = window.location.pathname.split('/');
+        const orgNameUrlPos = 1;
+        if (pathArray.length > orgNameUrlPos + 1 && pathArray[orgNameUrlPos] === 'organizations') {
+          this.activeOrg = pathArray[orgNameUrlPos + 1];
+        }
+      },
+    });
+  }
 });
+
