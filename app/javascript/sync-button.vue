473f00072f733470c6b14bec4f4673c40251f80e
Diego Fernández <difernandez@uc.cl>
2018-08-24 14:51:40 -0300

    feat(settings): add spinner pkg and move sync button to vue component

---
+++ app/javascript/sync-button.vue
@@ -0,0 +1,24 @@
+<template>
+  <form v-bind:action="url" accept-charset="UTF-8" method="post">
+    <input type="submit" name="commit" value="sync" data-disable-with="sync" class="card-extended__little-button">
+    <spinner class="sync-spinner"></spinner>
+  </form>
+</template>
+
+<script>
+  import Spinner from 'vue-simple-spinner';
+
+  export default {
+    props: ['url'],
+    data() {
+      return {
+        url: this.url
+      }
+    },
+    components: {
+      Spinner
+    },
+    methods: {
+    }
+  }
+</script>
