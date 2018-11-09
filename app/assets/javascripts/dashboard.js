c7579eab639e7ba032abe948b00978ce37917f29
Diego Fernández <difernandez@uc.cl>
2018-11-09 17:48:12 -0300

    feat(dashboard): stick corner on both scrolls

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -5,9 +5,9 @@ const offsetTop = 164;
 $(document).ready(() => {
   $(document).scroll(() => {
     if (document.documentElement.scrollTop > offsetTop) {
-      $('#sticky-head').css({ top: (document.documentElement.scrollTop) - offsetTop, position: 'relative' });
+      $('#sticky-head, #sticky-corner').css({ top: (document.documentElement.scrollTop) - offsetTop, position: 'relative' });
     } else {
-      $('#sticky-head').css({ top: 0, position: 'inherit' });
+      $('#sticky-head, #sticky-corner').css({ top: 0, position: 'inherit' });
     }
   });
 });
