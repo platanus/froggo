27cc7e502af60807c528e444e08339e3f4c954b7
fdominguezc <fdominguezc@uc.cl>
2018-11-15 16:55:59 -0300

    Revert "test(): adapt and create test"
    This reverts commit 8c19b948338e565c8955359a5d52c32bb30d04c8.

--- spec/values/correlation_matrix_spec.rb
+++ spec/values/correlation_matrix_spec.rb
@@ -5,8 +5,7 @@ RSpec.describe CorrelationMatrix, type: :class do
   let!(:repository) { create(:repository, organization: organization) }
 
   context 'when initialized with empty organization' do
-    let!(:current_user) { create(:github_user, gh_id: 8, login: 'gh_reviewer') }
-    subject { CorrelationMatrix.new(organization.id, nil, current_user.login) }
+    subject { CorrelationMatrix.new(organization.id, nil) }
     it 'has empty data' do
       expect(subject.data).to be_empty
     end
@@ -16,12 +15,11 @@ RSpec.describe CorrelationMatrix, type: :class do
     let!(:owner) { create(:github_user, gh_id: 3, login: 'gh_owner') }
     let!(:pull_requests) { [create(:pull_request, repository: repository, owner: owner)] }
     let!(:reviewer) { create(:github_user, gh_id: 8, login: 'gh_reviewer') }
-    let!(:current_user) { create(:github_user, gh_id: 8, login: 'gh_reviewer') }
     let!(:pr_relation) do
       create(:pull_request_relation, pull_request: pull_requests[0], github_user: reviewer)
     end
 
-    subject { CorrelationMatrix.new(organization.id, nil, current_user.login) }
+    subject { CorrelationMatrix.new(organization.id, nil) }
     it 'has zeros value' do
       expect(subject.data[[0, 0]]).to eq(0)
     end
@@ -31,13 +29,12 @@ RSpec.describe CorrelationMatrix, type: :class do
     let!(:owner) { create(:github_user, gh_id: 3, login: 'gh_owner') }
     let!(:reviewer) { create(:github_user, gh_id: 8, login: 'gh_reviewer') }
     let!(:pull_requests) { [create(:pull_request, repository: repository, owner: owner)] }
-    let!(:current_user) { create(:github_user, gh_id: 8, login: 'gh_owner') }
     before do
       create(:pull_request_relation, pull_request: pull_requests[0], github_user: owner)
       create(:pull_request_relation, pull_request: pull_requests[0], github_user: reviewer)
     end
 
-    subject { CorrelationMatrix.new(organization.id, nil, current_user.login) }
+    subject { CorrelationMatrix.new(organization.id, nil) }
 
     it 'should change data value if exist' do
       expect do
@@ -77,52 +74,19 @@ RSpec.describe CorrelationMatrix, type: :class do
       create(:organization_membership, organization: organization, github_user: other)
     end
     let!(:pull_requests) { [create(:pull_request, repository: repository, owner: owner)] }
-    let!(:current_user) { create(:github_user, gh_id: 8, login: 'gh_reviewer') }
+
     before do
       create(:pull_request_relation, pull_request: pull_requests[0], github_user_id: owner.id)
       create(:pull_request_relation, pull_request: pull_requests[0], github_user_id: reviewer.id)
       create(:pull_request_relation, pull_request: pull_requests[0], github_user_id: other.id)
     end
 
-    subject do
-      CorrelationMatrix.new(organization.id, [owner.gh_id, reviewer.gh_id], current_user.login)
-    end
+    subject { CorrelationMatrix.new(organization.id, [owner.gh_id, reviewer.gh_id]) }
 
     it 'should ignore selected users' do
       subject.fill_matrix
       expect(organization.members.length).to eq(3)
       expect(subject.selected_users.length).to eq(2)
-    end
-  end
-
-  context 'order selected users' do
-    let!(:owner) { create(:github_user, gh_id: 3, login: 'gh_owner') }
-    let!(:reviewer) { create(:github_user, gh_id: 8, login: 'gh_reviewer') }
-    let!(:other) { create(:github_user, gh_id: 9, login: 'gh_other') }
-    let!(:owner_membership) do
-      create(:organization_membership, organization: organization, github_user: owner)
-    end
-    let!(:reviewer_membership) do
-      create(:organization_membership, organization: organization, github_user: reviewer)
-    end
-    let!(:other_membership) do
-      create(:organization_membership, organization: organization, github_user: other)
-    end
-    let!(:pull_requests) { [create(:pull_request, repository: repository, owner: owner)] }
-    let!(:current_user) { create(:github_user, gh_id: 8, login: 'gh_owner') }
-    before do
-      create(:pull_request_relation, pull_request: pull_requests[0], github_user_id: owner.id)
-      create(:pull_request_relation, pull_request: pull_requests[0], github_user_id: reviewer.id)
-      create(:pull_request_relation, pull_request: pull_requests[0], github_user_id: other.id)
-    end
-
-    subject do
-      CorrelationMatrix.new(organization.id, [owner.gh_id, reviewer.gh_id], current_user.login)
-    end
-
-    it 'should place current user first' do
-      subject.fill_matrix
-      expect(subject.selected_users.first.login).to eq('gh_owner')
     end
   end
 end
