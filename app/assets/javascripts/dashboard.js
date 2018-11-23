6365aa157da72fd6528ceaeed5f2da717ce3b74f
Diego Fernández <difernandez@uc.cl>
2018-11-23 10:47:24 -0300

    fix(dashboard): fix sticky header in public dashboard

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -5,6 +5,9 @@ const autoscrollInterval = 10000;
 
 $(document).ready(() => {
   $(document).scroll(() => {
+    if ($('.card-extended--fullscreen')[0]) {
+      return;
+    }
     if (document.documentElement.scrollTop > offsetTop) {
       $('#sticky-head, #sticky-corner').css({ top: (document.documentElement.scrollTop) - offsetTop, position: 'relative' });
     } else {
@@ -15,19 +18,19 @@ $(document).ready(() => {
 
 $(document).ready(() => {
   let upToDown = true;
-  setInterval(() => {
-    const fullscreenDashboard = $('.card-extended--fullscreen')[0];
-    if (fullscreenDashboard.scrollTop >= fullscreenDashboard.scrollHeight - fullscreenDashboard.offsetHeight) {
-      upToDown = false;
-    } else if (fullscreenDashboard.scrollTop === 0) {
-      upToDown = true;
-    }
-    if (window.location.href.includes('public')) {
-      if (upToDown) {
-        fullscreenDashboard.scrollBy(0, fullscreenDashboard.offsetHeight);
-      } else {
-        fullscreenDashboard.scrollBy(0, -fullscreenDashboard.offsetHeight);
+  if (window.location.href.includes('public')) {
+    setInterval(() => {
+      const viewport = document.documentElement;
+      if (viewport.scrollTop >= viewport.scrollHeight - viewport.clientHeight) {
+        upToDown = false;
+      } else if (viewport.scrollTop === 0) {
+        upToDown = true;
       }
-    }
-  }, autoscrollInterval);
+      if (upToDown) {
+        viewport.scrollBy(0, viewport.clientHeight);
+      } else {
+        viewport.scrollBy(0, -viewport.clientHeight);
+      }
+    }, autoscrollInterval);
+  }
 });
