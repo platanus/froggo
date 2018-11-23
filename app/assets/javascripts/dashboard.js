dc0fab84b85ef11bd3a59c395c33d4be41c12cff
Diego Fernández <difernandez@uc.cl>
2018-11-23 09:06:04 -0300

    feat(dashboard): autoscroll public dashboard

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,6 +1,7 @@
 /* global document, $ */
 
 const offsetTop = 164;
+const autoscrollInterval = 10000;
 
 $(document).ready(() => {
   $(document).scroll(() => {
@@ -10,4 +11,23 @@ $(document).ready(() => {
       $('#sticky-head, #sticky-corner').css({ top: 0, position: 'inherit' });
     }
   });
+});
+
+$(document).ready(() => {
+  let upToDown = true;
+  setInterval(() => {
+    const fullscreenDashboard = $('.card-extended--fullscreen')[0];
+    if (fullscreenDashboard.scrollTop >= fullscreenDashboard.scrollHeight - fullscreenDashboard.offsetHeight) {
+      upToDown = false;
+    } else if (fullscreenDashboard.scrollTop === 0) {
+      upToDown = true;
+    }
+    if (window.location.href.includes('public')) {
+      if (upToDown) {
+        fullscreenDashboard.scrollBy(0, fullscreenDashboard.offsetHeight);
+      } else {
+        fullscreenDashboard.scrollBy(0, -fullscreenDashboard.offsetHeight);
+      }
+    }
+  }, autoscrollInterval);
 });
