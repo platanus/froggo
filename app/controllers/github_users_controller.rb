92a25561912bda7b489819ed7aff5f87edc47c4f
Stefano Gioia <stefanogch@gmail.com>
2019-02-06 17:37:30 -0300

    feat(profile-pages): selectable teams in a user's profile page

--- app/controllers/github_users_controller.rb
+++ app/controllers/github_users_controller.rb
@@ -4,5 +4,9 @@ class GithubUsersController < ApplicationController
     # Set github_session so that the header partial
     # has access to its data.
     @github_session = github_session
+    @teams =
+      GithubUserService
+        .new
+        .fetch_teams_for_user(@github_user.login, @github_session.client)
   end
 end
---
+++ app/helpers/github_users_helper.rb
@@ -0,0 +1,10 @@
+module GithubUsersHelper
+  # Converts a list of teams, as retrived by 
+  # GithubUserService#fetch_teams_for_user to a list of teams compatible
+  # with the `clickable-dropdown` Vue component.
+  def convert_teams_to_clickable_dropdown_items(teams)
+    teams.map do |team| 
+      { id: team[:id], name: team[:name] }
+    end
+  end
+end
