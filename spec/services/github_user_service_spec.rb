1a4cc75e2916824f7109248edbd95df82fafafa2
Stefano Gioia <stefanogch@gmail.com>
2019-02-07 10:57:04 -0300

    feat(profile-pages): test team fetching

--- spec/services/github_user_service_spec.rb
+++ spec/services/github_user_service_spec.rb
@@ -20,6 +20,7 @@ describe GithubUserService do
            name: '')
   end
 
+
   def build(*_args)
     described_class.new(*_args)
   end
@@ -33,6 +34,72 @@ describe GithubUserService do
     it 'find an existing user in BD' do
       find = build.find_or_create(user2)
       expect(find).to eq(gh_user)
+    end
+  end
+
+  context 'fetch_teams_for_user' do
+    let (:organization_1) do
+      organization = double('organization_1')
+      allow(organization).to receive(:login) { 'organization_1_login' }
+      organization
+    end
+
+    let (:organization_2) do
+      organization = double('organization_2')
+      allow(organization).to receive(:login) { 'organization_2_login' }
+      organization
+    end
+
+    let (:organizations) do
+      [ organization_1, organization_2 ]
+    end
+
+    let (:team_1_data) do
+      { name: 'team_1', id: 'team_1_id' }
+    end
+
+    let (:team_1) do
+      team = double('team_1', **team_1_data)
+      allow(team).to receive(:to_attrs) { team_1_data }
+      team
+    end
+
+    let (:team_2_data) do
+      { name: 'team_2', id: 'team_2_id' }
+    end
+
+    let (:team_2) do
+      team = double('team_2', **team_2_data)
+      allow(team).to receive(:to_attrs) { team_2_data }
+      team
+    end
+
+    let (:teams) do
+      [ team_1, team_2 ]
+    end
+
+    let (:octokit_client_with_teams) do
+      client = double('octokit-client')
+      allow(client).to receive(:organizations) { organizations }
+      allow(client).to receive(:organization_teams) { teams }
+      client
+    end
+
+    let (:octokit_client_with_no_orgs) do
+      client = double('octokit-client')
+      allow(client).to receive(:organizations) { [] }
+      client
+    end
+
+    it 'returns empty list when no organizations found' do
+      teams = build.fetch_teams_for_user('login', octokit_client_with_no_orgs)
+      expect(teams).to be_empty
+    end
+
+    it 'returns teams' do
+      teams = build.fetch_teams_for_user('login', octokit_client_with_teams)
+      expect(teams[0]).to equal(team_1_data)
+      expect(teams[1]).to equal(team_2_data)
     end
   end
 end
