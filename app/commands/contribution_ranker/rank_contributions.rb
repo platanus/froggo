e15a7f2bbc9c913c00b1de4e61aa06a56df22178
Stefano Gioia <stefanogch@gmail.com>
2019-02-12 10:47:39 -0300

    style(individual-score): use object state instead of method arguments

--- app/commands/contribution_ranker/rank_contributions.rb
+++ app/commands/contribution_ranker/rank_contributions.rb
@@ -1,8 +1,9 @@
 class ContributionRanker::RankContributions <
   PowerTypes::Command.new(:users_contributions, :limit)
   def perform
-    validate_users_contributions! @users_contributions
-    rank(with_self_contributions_first(@users_contributions)).first(@limit)
+    @amount_of_users = infer_amount_of_users
+    validate_users_contributions!
+    rank(with_self_contributions_first).first(@limit)
   end
 
   private
@@ -19,31 +20,29 @@ class ContributionRanker::RankContributions <
       .map(&:first)
   end
 
-  def validate_users_contributions!(users_contributions)
-    amount_of_users = infer_amount_of_users users_contributions
-    (0...amount_of_users).each do |i|
-      (0...amount_of_users).each do |j|
+  def validate_users_contributions!
+    (0...@amount_of_users).each do |i|
+      (0...@amount_of_users).each do |j|
         raise(ArgumentError, "Data not found at (#{i}, #{j})") \
-          unless users_contributions[[i, j]]
+          unless @users_contributions[[i, j]]
       end
     end
   end
 
-  def infer_amount_of_users(users_contributions)
+  def infer_amount_of_users
     max_index = -1
-    users_contributions.keys.each do |x_and_y|
+    @users_contributions.keys.each do |x_and_y|
       max_index = [max_index, x_and_y.max].max
     end
     max_index + 1
   end
 
-  def with_self_contributions_first(users_contributions)
+  def with_self_contributions_first
     sorted_users_contributions = {}
-    amount_of_users = infer_amount_of_users users_contributions
-    (0...amount_of_users).each do |x|
-      sorted_users_contributions[x] = [users_contributions[[x, x]]]
-      (0...amount_of_users).each do |y|
-        sorted_users_contributions[x] << users_contributions[[x, y]] if x != y
+    (0...@amount_of_users).each do |x|
+      sorted_users_contributions[x] = [@users_contributions[[x, x]]]
+      (0...@amount_of_users).each do |y|
+        sorted_users_contributions[x] << @users_contributions[[x, y]] if x != y
       end
     end
     sorted_users_contributions
