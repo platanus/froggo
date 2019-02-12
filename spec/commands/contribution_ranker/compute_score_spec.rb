fbf3e0dc1c35939c0559f7e293582e7abe10aabd
Stefano Gioia <stefanogch@gmail.com>
2019-02-12 10:56:43 -0300

    feat(individual-score): better tests

--- spec/commands/contribution_ranker/compute_score_spec.rb
+++ spec/commands/contribution_ranker/compute_score_spec.rb
@@ -6,16 +6,18 @@ describe ContributionRanker::ComputeScore do
   end
 
   context 'when user contributes nothing' do
-    it 'returns 0 when there are no other users' do
-      expect(perform(self_reviewed_prs: 30, per_user_reviews: [])).to eq(0)
-    end
-    it 'returns 0 when there are multiple other users' do
-      expect(perform(self_reviewed_prs: 30, per_user_reviews: [0, 0, 0])).to eq(0)
+    it do
+      expect(perform(self_reviewed_prs: 30, per_user_reviews: [0, 0, 0]))
+        .to eq(0)
     end
   end
 
+  context 'when there are no other users' do
+    it { expect(perform(self_reviewed_prs: 10, per_user_reviews: [])).to eq(0) }
+  end
+
   context 'when user contributes something' do
-    it 'is not 0' do
+    it do
       expect(perform(self_reviewed_prs: 30, per_user_reviews: [1, 2, 0]))
         .not_to eq(0)
     end
