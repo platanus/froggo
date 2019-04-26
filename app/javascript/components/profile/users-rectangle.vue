6d8df9537d4002a4a4d4efd7ece2d38d12883354
René Morales <rene.morales.sanchez@gmail.com>
2019-04-26 16:51:27 -0400

    chore(profile/users-rectangle): removes small profile component

--- app/javascript/components/profile/users-rectangle.vue
+++ app/javascript/components/profile/users-rectangle.vue
@@ -4,34 +4,29 @@
     <div class="profile-recommendations-users__title">
       {{ title }}
     </div>
-        <small-profile
-          v-else
-          v-for="user in recommendations[type]"
-          :key="user.id"
-          :github-login="user.login"
-          :link-to-profile="user.html_url"
-          :avatar-url="user.avatar_url"
-        >
-        </small-profile>
     <div class="profile-recommendations-users__users">
       <div v-if="beingFetched" class="loading-icon"></div>
+      <div
+        v-else
+        class="profile-recommendations-users__user"
+        v-for="user in recommendations[type]"
+        :key="user.id">
+        <img class="profile-recommendations-users__picture" :src="user.avatar_url" />
+        <div class="profile-recommendations-users__username">
+          {{ user.login }}
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import SmallProfile from './small.vue';
-
 export default {
   props: {
     title: String,
     recommendations: Object,
     type: String,
     beingFetched: Boolean,
-  },
-  components: {
-    SmallProfile,
   },
 };
 </script>
