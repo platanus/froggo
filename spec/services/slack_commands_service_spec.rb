e8df0f26f6c8bc90784957db48c1bd70e13c145e
isidoravs <isvizcaya@uc.cl>
2019-05-17 11:30:06 -0400

    feat(slack): send summary of user statistics with /summary command

--- spec/services/slack_commands_service_spec.rb
+++ spec/services/slack_commands_service_spec.rb
@@ -17,12 +17,19 @@ describe SlackCommandsService do
       ]
     }
   end
+  let(:statistics) { { obedient: 1, indifferent: 0, rebel: 2 } }
+  let(:statistics_count) { { obedient_count: 1, indifferent_count: 0, rebel_count: 2 } }
 
   before do
     allow(GetRecommendations).to receive(:for)
       .with(github_user: github_user,
             organization: organization)
       .and_return(recommendations)
+
+    allow(ComputeUserStatistics).to receive(:for)
+      .with(github_user_id: github_user.id,
+            organization_id: organization.id)
+      .and_return(statistics)
 
     allow(I18n).to receive(:t)
       .with('messages.slack.recommended_users', users: 'blackjid, gmq, iobaixas')
@@ -33,12 +40,20 @@ describe SlackCommandsService do
       .and_return('croak!')
 
     allow(I18n).to receive(:t)
-      .with('messages.slack.wrong_params')
+      .with('messages.slack.next_pr_wrong_params')
       .and_return('Avíspate')
 
     allow(I18n).to receive(:t)
       .with('messages.slack.no_recommendations')
       .and_return('No puedes ver esto')
+
+    allow(I18n).to receive(:t)
+      .with('messages.slack.summary', **statistics_count)
+      .and_return('Puntos: 1 obediencia, 0 indiferencia, 2 rebeldía')
+
+    allow(I18n).to receive(:t)
+      .with('messages.slack.summary_wrong_params')
+      .and_return('Avíspate')
   end
 
   context 'command is /froggo' do
@@ -102,10 +117,44 @@ describe SlackCommandsService do
     it { expect(service.reply).to eq('No puedes ver esto') }
   end
 
-  context 'command is /next_pr and user doesn\'t belong to default team' do
-    let(:params) { { 'command' => '/next_pr', 'text' => 'isidoravs platanus' } }
+  context 'command is /summary and params are correct' do
+    let(:params) { { 'command' => '/summary', 'text' => 'isidoravs platanus' } }
+    let!(:membership) do
+      create(:organization_membership, github_user_id: github_user.id,
+                                       organization_id: organization.id,
+                                       is_member_of_default_team: true)
+    end
 
-    before do
+    it { expect(service.reply).to eq('Puntos: 1 obediencia, 0 indiferencia, 2 rebeldía') }
+  end
+
+  context 'command is /summary and there\'s no organization' do
+    let(:params) { { 'command' => '/summary', 'text' => 'isidoravs' } }
+
+    it { expect(service.reply).to eq('Avíspate') }
+  end
+
+  context 'command is /summary and there\'s no content' do
+    let(:params) { { 'command' => '/summary', 'text' => '' } }
+
+    it { expect(service.reply).to eq('Avíspate') }
+  end
+
+  context 'command is /summary and organization doesn\'t exist' do
+    let(:params) { { 'command' => '/summary', 'text' => 'isidoravs bananus' } }
+
+    it { expect(service.reply).to eq('Avíspate') }
+  end
+
+  context 'command is /summary and user doesn\'t exist' do
+    let(:params) { { 'command' => '/summary', 'text' => 'billgates platanus' } }
+
+    it { expect(service.reply).to eq('Avíspate') }
+  end
+
+  context 'command is /summary and user doesn\'t belong to default team' do
+    let(:params) { { 'command' => '/summary', 'text' => 'isidoravs platanus' } }
+    let!(:membership) do
       create(:organization_membership, github_user_id: github_user.id,
                                        organization_id: organization.id,
                                        is_member_of_default_team: false)
@@ -114,8 +163,8 @@ describe SlackCommandsService do
     it { expect(service.reply).to eq('No puedes ver esto') }
   end
 
-  context 'command is /next_pr and user doesn\'t belong to organization' do
-    let(:params) { { 'command' => '/next_pr', 'text' => 'isidoravs budacom' } }
+  context 'command is /summary and user doesn\'t belong to organization' do
+    let(:params) { { 'command' => '/summary', 'text' => 'isidoravs budacom' } }
     let(:organization) { create(:organization, login: 'budacom') }
 
     it { expect(service.reply).to eq('No puedes ver esto') }
