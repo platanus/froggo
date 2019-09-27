1a9520c3d7e09bce73dd8310041afaf27b7337e7
Diego Casas <dacasas@uc.cl>
2019-09-27 18:05:48 -0300

    test(model/OrganizationSyncSpec): prefer change { sync.state }

--- spec/models/organization_sync_spec.rb
+++ spec/models/organization_sync_spec.rb
@@ -10,28 +10,28 @@ RSpec.describe OrganizationSync, type: :model do
   describe 'aasm' do
     context '#execute' do
       it '#execute changes state from created to executing' do
-        expect { sync.execute }.to change(sync, :state).from('created').to('executing')
+        expect { sync.execute }.to change{ sync.state }.from('created').to('executing')
       end
 
-      context '#when execute completed' do
+      context 'when execute completed' do
         before do
           sync.execute
           sync.complete
         end
 
         it '#execute changes state from completed to executing' do
-          expect { sync.execute }.to change(sync, :state).from('completed').to('executing')
+          expect { sync.execute }.to change{ sync.state }.from('completed').to('executing')
         end
       end
 
-      context '#when execute failed' do
+      context 'when execute failed' do
         before do
           sync.execute
           sync.fail
         end
 
         it '#execute changes state from failed to executing' do
-          expect { sync.execute }.to change(sync, :state).from('failed').to('executing')
+          expect { sync.execute }.to change{ sync.state }.from('failed').to('executing')
         end
       end
     end
@@ -41,7 +41,7 @@ RSpec.describe OrganizationSync, type: :model do
         sync.execute
       end
       it '#fail changes state from executing to failed' do
-        expect { sync.fail }.to change(sync, :state).from('executing').to('failed')
+        expect { sync.fail }.to change{ sync.state }.from('executing').to('failed')
       end
     end
 
@@ -50,7 +50,7 @@ RSpec.describe OrganizationSync, type: :model do
         sync.execute
       end
       it '#complete changes state from executing to completed' do
-        expect { sync.complete }.to change(sync, :state).from('executing').to('completed')
+        expect { sync.complete }.to change{ sync.state }.from('executing').to('completed')
       end
     end
   end
