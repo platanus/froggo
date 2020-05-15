cd7b9708c33c7f7c583aeef6b7be1d736a09797c
Gabriel Lyon <glyon1@uc.cl>
2020-05-15 10:38:53 -0400

    chore(webpack-enviroment): update and include file for vue-loader v15

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,19 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+
+// environment.loaders.delete('nodeModules');
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+environment.config.merge({
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+});
+
+debugger;
 
 module.exports = environment
