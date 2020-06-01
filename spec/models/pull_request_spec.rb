17bb781548f31404912d21ad386d50112ffae7c2
Gabriel Lyon <glyon1@uc.cl>
2020-06-01 10:38:47 -0400

    test(models): pull request and pull request review request update to use is_expected and optional values

--- spec/models/pull_request_spec.rb
+++ spec/models/pull_request_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe PullRequest, type: :model do
   describe 'validations' do
-    it { should validate_presence_of :gh_id }
-    it { should validate_presence_of :pr_state }
+    it { is_expected.to validate_presence_of :gh_id }
+    it { is_expected.to validate_presence_of :pr_state }
   end
 
   describe 'relationships' do
-    it { should belong_to(:repository) }
-    it { should belong_to(:owner) }
-    it { should belong_to(:merged_by) }
+    it { is_expected.to belong_to(:repository) }
+    it { is_expected.to belong_to(:owner) }
+    it { is_expected.to belong_to(:merged_by).optional(true) }
   end
 end
