848b5c10c773c2385fdd7b810c57242bff77af34
Gabriel Lyon <glyon1@uc.cl>
2020-06-01 10:29:07 -0400

    feat(data_schema): define

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,9 @@ ActiveRecord::Schema.define(version: 2019_05_02_193942) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "data_migrations", primary_key: "version", id: :string, force: :cascade do |t|
+  end
+
   create_table "github_users", force: :cascade do |t|
     t.string "avatar_url"
     t.string "email"
