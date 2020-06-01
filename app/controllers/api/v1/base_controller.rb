e69bf0420bae8ce59b6d4860010ada7dbf328170
Gabriel Lyon <glyon1@uc.cl>
2020-06-01 16:22:57 -0400

    feat(api-base-controllers): delete old base_controller and update new one

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::BaseController < ApplicationController
-  include ApiErrorConcern
+  include Api::ErrorConcern
+  include Api::Versioned
 
   self.responder = ApiResponder
 
+  skip_before_action :verify_authenticity_token
   respond_to :json
 end
