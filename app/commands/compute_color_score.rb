fec05e0ce8be26d5073fa8ee23ec06c47d63d75e
Javier G. Montoya S <jgmontoya@pm.me>
2020-07-24 19:42:18 -0400

    feat(commands/compute_color_score): add command to compute the score used for coloring
    also add it's respective test

---
+++ app/commands/compute_color_score.rb
@@ -0,0 +1,43 @@
+class ComputeColorScore < PowerTypes::Command.new(:user_id, :other_users_ids, :pr_relations,
+                                                  review_month_limit: nil)
+  REVIEW_MONTH_LIMIT_DEFAULT = 9
+
+  def perform
+    scores = {}
+    @other_users_ids.each do |other_user_id|
+      next if @user_id == other_user_id
+
+      scores[other_user_id] = score_for_other_user(other_user_id)
+    end
+    scores
+  end
+
+  private
+
+  def review_month_limit
+    @review_month_limit ||= REVIEW_MONTH_LIMIT_DEFAULT
+  end
+
+  def score_for_other_user(other_user_id)
+    return -1 if mean_prs_sent.zero?
+
+    (reviews_per_user[other_user_id] || 0).to_f / mean_prs_sent
+  end
+
+  def mean_prs_sent
+    @mean_prs_sent ||= compute_mean_prs_sent
+  end
+
+  def compute_mean_prs_sent
+    reviews_per_user.empty? ? 0.0 : reviews_per_user.values.sum.to_f / reviews_per_user.length
+  end
+
+  def reviews_per_user
+    @reviews_per_user ||= @pr_relations.within_month_limit(review_month_limit)
+                                       .where(target_user_id: @user_id)
+                                       .where(github_user_id: @other_users_ids)
+                                       .where.not(github_user_id: @user_id)
+                                       .group(:github_user_id)
+                                       .count
+  end
+end
---
+++ spec/commands/compute_color_score_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe ComputeColorScore do
+  def perform(*args)
+    described_class.for(*args)
+  end
+
+  def perform_with_predefined_args
+    perform(
+      user_id: user.id,
+      other_users_ids: other_users.pluck(:id),
+      pr_relations: pr_relations
+    )
+  end
+
+  let(:user) { create(:github_user) }
+  let(:other_users_ids) { [2, 3, 4, 5] }
+  let(:other_users) do
+    create_list(:github_user, 4) do |user, i|
+      user.id = other_users_ids[i]
+      user.save!
+    end
+  end
+
+  context 'with  different user interactions' do
+    let(:pr_relations_array) do
+      create_list(:pull_request_relation, 6) do |pr_relation, i|
+        pr_relation.target_user_id = user.id
+        pr_relation.github_user_id = 1 + 2**(i.to_f / 3).ceil
+        pr_relation.save!
+      end
+    end
+    let(:pr_relations) { PullRequestRelation.where(id: pr_relations_array.pluck(:id)) }
+
+    it 'returns the proper score' do
+      expect(perform_with_predefined_args).to eq({ 2 => 0.5, 3 => 1.5, 4 => 0.0, 5 => 1.0 })
+    end
+  end
+
+  context 'without user interactions' do
+    let(:pr_relations_array) { Array.new(5) { create(:pull_request_relation) } }
+    let(:pr_relations) { PullRequestRelation.where(id: pr_relations_array.pluck(:id)) }
+
+    it 'returns the proper placeholder scores' do
+      expect(perform_with_predefined_args).to eq({ 2 => -1, 3 => -1, 4 => -1, 5 => -1 })
+    end
+  end
+end
