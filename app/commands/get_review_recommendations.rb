889948788665ccee7cbb498440b049d44fba064c
Javier G. Montoya S <jgmontoya@pm.me>
2020-08-05 11:40:21 -0400

    refactor(commands/get_review_recommendations): improve legibility

--- app/commands/get_review_recommendations.rb
+++ app/commands/get_review_recommendations.rb
@@ -2,20 +2,21 @@ class GetReviewRecommendations < PowerTypes::Command.new(:github_user_id, :other
   NUMBER_OF_RECOMMENDATIONS = 3
 
   def perform
-    users_with_score = other_users_with_score
     {
-      best: best_recommendations(users_with_score, number_of_best_recommendations),
-      worst: worst_recommendations(users_with_score, number_of_worst_recommendations),
-      all: users_with_score
+      best: best_recommendations(other_users_with_score, number_of_best_recommendations),
+      worst: worst_recommendations(other_users_with_score, number_of_worst_recommendations),
+      all: other_users_with_score
     }
   end
 
   private
 
   def color_scores
-    pr_relations = PullRequestRelation.review_relations
-    ComputeColorScore.for(user_id: @github_user_id, other_users_ids: @other_users_ids,
-                          pr_relations: pr_relations)
+    @color_scores ||= begin
+      pr_relations = PullRequestRelation.review_relations
+      ComputeColorScore.for(user_id: @github_user_id, other_users_ids: @other_users_ids,
+                            pr_relations: pr_relations)
+    end
   end
 
   def best_recommendations(sorted_map, number_of_recommendations)
@@ -27,23 +28,26 @@ class GetReviewRecommendations < PowerTypes::Command.new(:github_user_id, :other
   end
 
   def other_users_with_score
-    scores = color_scores
-    result = {}
-    GithubUser.where(id: @other_users_ids).map do |user|
-      result[user.id] = user.as_json.merge(score: scores[user.id])
+    @other_users_with_score ||= begin
+      result = {}
+      GithubUser.where(id: @other_users_ids).map do |user|
+        result[user.id] = user.as_json.merge(score: color_scores[user.id])
+      end
+      result.sort_by { |_, user| user[:score] }
     end
-    result.sort_by { |_, user| user[:score] }
   end
 
   def number_of_best_recommendations
-    num_of_users = @other_users_ids.length
-    return NUMBER_OF_RECOMMENDATIONS if num_of_users > NUMBER_OF_RECOMMENDATIONS
+    @number_of_best_recommendations ||= begin
+      num_of_users = @other_users_ids.length
+      return NUMBER_OF_RECOMMENDATIONS if num_of_users > NUMBER_OF_RECOMMENDATIONS
 
-    [num_of_users - 1, 1].max
+      [num_of_users - 1, 0].max
+    end
   end
 
   def number_of_worst_recommendations
     num_of_users = @other_users_ids.length
-    [NUMBER_OF_RECOMMENDATIONS, [0, num_of_users - number_of_best_recommendations].max].min
+    [NUMBER_OF_RECOMMENDATIONS, num_of_users - number_of_best_recommendations].min
   end
 end
