c4bccd86546bf95fdc6f7290ebe5695517bdfbeb
Javier G. Montoya S <jgmontoya@pm.me>
2020-08-06 15:14:40 -0400

    fix(commands/get_review_recommendation): add symbol access to returned hashes

--- app/commands/get_review_recommendations.rb
+++ app/commands/get_review_recommendations.rb
@@ -29,7 +29,9 @@ class GetReviewRecommendations < PowerTypes::Command.new(:github_user_id, :other
 
   def other_users_with_score
     @other_users_with_score ||= GithubUser.where(id: @other_users_ids).map do |user|
-                                  user.as_json.merge(score: color_scores[user.id])
+                                  user.as_json.with_indifferent_access.merge(
+                                    score: color_scores[user.id]
+                                  )
                                 end.sort_by { |user| user[:score] }
   end
 
