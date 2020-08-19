f49fbbc33dd388b986539c88bc61b578703257aa
miguelramirezv <mnramirez1@uc.cl>
2020-08-19 12:00:13 -0400

    fixup! feat(pull-request-controller): add basic index

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,6 +1,9 @@
 class PullRequestsController < ApplicationController
   def index
-    @pull_requests = PullRequest.by_organizations([organization.id])
+    @pull_requests = PullRequest.by_organizations([organization.id]).limit(20)
+    @serialized_pull_requests = ActiveModel::Serializer::CollectionSerializer.new(
+      @pull_requests, each_serializer: PullRequestSerializer
+    )
   end
 
   private
@@ -8,7 +11,7 @@ class PullRequestsController < ApplicationController
   def organization
     @organization ||= Organization.find_by(login: organization_name)
   end
-  
+
   def organization_name
     params.require(:organization_name)
   end
