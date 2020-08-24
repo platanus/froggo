6c2c5b26a6805fadae170bf3e267f0574778abc1
Jeronimo Salazar Salas <jcsalazar@uc.cl>
2020-08-24 12:42:59 -0400

    feat(user-metrics): Add Vuex component to request and receive metrics information

--- app/javascript/store/modules/profile/actions.js
+++ app/javascript/store/modules/profile/actions.js
@@ -5,6 +5,7 @@ import {
   COMPUTE_STATISTICS,
   COMPUTE_RECOMMENDATIONS,
   PROCESS_NEW_TEAM,
+  COMPUTE_PROFILE_PR_INFORMATION,
 } from '../../action-types';
 
 import {
@@ -12,6 +13,9 @@ import {
   RECOMMENDATIONS_FETCH_ERROR,
   RECOMMENDATIONS_RECEIVED,
   START_FETCHING_RECOMMENDATIONS,
+  START_FETCHING_PR_INFORMATION,
+  PROFILE_PR_INFORMATION_RECEIVED,
+  PROFILE_PR_INFORMATION_FETCH_ERROR,
 } from '../../mutation-types';
 
 export default {
@@ -23,6 +27,7 @@ export default {
       githubUserLogin,
     });
     dispatch(COMPUTE_RECOMMENDATIONS, { teamId, githubUserLogin });
+    dispatch(COMPUTE_PROFILE_PR_INFORMATION, { githubUserLogin });
   },
 
   [COMPUTE_RECOMMENDATIONS]({ commit }, { teamId, githubUserLogin, monthLimit }) {
@@ -38,6 +43,22 @@ export default {
       })
       .catch(error => {
         commit(RECOMMENDATIONS_FETCH_ERROR, error);
+      });
+  },
+
+  [COMPUTE_PROFILE_PR_INFORMATION]({ commit }, { githubUserLogin, monthLimit }) {
+    commit(START_FETCHING_PR_INFORMATION);
+    axios
+      .get(`/api/users/${githubUserLogin}/pull_requests_information`, {
+        params: decamelizeKeys({
+          monthLimit,
+        }),
+      })
+      .then(response => {
+        commit(PROFILE_PR_INFORMATION_RECEIVED, response.data.response.metrics);
+      })
+      .catch(error => {
+        commit(PROFILE_PR_INFORMATION_FETCH_ERROR, error);
       });
   },
 };
--- app/javascript/store/modules/profile/state.js
+++ app/javascript/store/modules/profile/state.js
@@ -2,4 +2,7 @@ export default {
   selectedTeamId: null,
   fetchingRecommendations: false,
   recommendations: null,
+  fetchingPullRequestInformation: false,
+  pullRequestInformation: null,
+  userMeanTime: null,
 };
