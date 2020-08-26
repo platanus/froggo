9ee2646670d366d7c186c80f034128bb62d0ad59
pfarellano <pfarellano@uc.cl>
2020-08-26 14:44:50 -0400

    feat(FroggoTeamsForm): add show method to controller

--- app/controllers/froggo_teams_controller.rb
+++ app/controllers/froggo_teams_controller.rb
@@ -1,12 +1,21 @@
 class FroggoTeamsController < ApplicationController
+  before_action :authenticate_github_user
   def new
     @organization = Organization.find(params[:organization_id])
     @users = @organization.members
+    @github_user = github_user
   end
 
   def index
     user = GithubUser.find(params[:github_user_id])
     @user_teams = user.froggo_teams
     @organizations = user.organizations
+    @github_user = github_user
+  end
+
+  def show
+    @froggo_team = FroggoTeam.find(params[:id])
+    @froggo_team_members = @froggo_team.github_users
+    @github_user = github_user
   end
 end
