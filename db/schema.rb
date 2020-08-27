ee3992a6248cb47b6d6f8fcf1abf980344ddc314
Jeronimo Salazar Salas <jcsalazar@uc.cl>
2020-08-27 16:20:36 -0400

    feat(user-metrics): add pull request approval as PullRequestReview attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_190918) do
+ActiveRecord::Schema.define(version: 2020_08_25_200237) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -170,6 +171,7 @@ ActiveRecord::Schema.define(version: 2020_08_19_190918) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "recommendation_behaviour", default: "not_defined"
+    t.datetime "approved_at"
     t.index ["github_user_id"], name: "index_pull_request_reviews_on_github_user_id"
     t.index ["pull_request_id"], name: "index_pull_request_reviews_on_pull_request_id"
   end
