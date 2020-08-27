52370897d89355080e30f81815d00a773ac23a56
pfarellano <pfarellano@uc.cl>
2020-08-27 15:35:38 -0400

    refactor(FroggoTeamsForm): refactor vuex create method

--- app/javascript/store/modules/froggo_team/actions.js
+++ app/javascript/store/modules/froggo_team/actions.js
@@ -1,29 +1,12 @@
 import axios from 'axios';
-import * as humps from 'humps';
-
+import { decamelizeKeys } from 'humps';
 import {
   CREATE_NEW_FROGGO_TEAM,
 } from '../../action-types';
 
 export default {
-  [CREATE_NEW_FROGGO_TEAM](_, { name, organizationId, users }) {
-    axios.post(
-      `/api/organizations/${organizationId}/froggo_teams`,
-      { name },
-      { headers: { 'Content-Type': 'application/json' } },
-    )
-      .then(async (response) => {
-        const { data: { id } } = response;
-
-        return Promise.all(users.map(user => axios.post(`/api/froggo_teams/${id}/add_member`,
-          humps.decamelizeKeys({ githubLogin: user.login }))));
-      })
-      .then((response) => {
-        alert('se creó el grupo correctamente');
-        window.location.href = `/froggo_teams/${response[0].data.id}`;
-      })
-      .catch(() => {
-        alert('no se pudo crear el grupo correctamente');
-      });
+  [CREATE_NEW_FROGGO_TEAM](_, { name, organizationId, userIds }) {
+    return axios.post(
+      `/api/organizations/${organizationId}/froggo_teams`, decamelizeKeys({ name, newMembersIds: userIds }));
   },
 };
