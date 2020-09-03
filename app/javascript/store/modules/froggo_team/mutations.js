8c167f9b852e349c2e5e50e92069cda0e91997ad
pfarellano <pfarellano@uc.cl>
2020-09-03 11:11:09 -0400

    feat(FroggoTeamsForm): add states and mutations for members

--- app/javascript/store/modules/froggo_team/mutations.js
+++ app/javascript/store/modules/froggo_team/mutations.js
@@ -1,9 +1,25 @@
 import {
   TEAM_NAME,
+  LOAD_MEMBERS,
+  ADD_MEMBER,
+  REMOVE_MEMBER,
 } from '../../mutation-types';
 
 export default {
   [TEAM_NAME](state, name) {
     state.teamName = name;
+  },
+  [LOAD_MEMBERS](state, { members, possibleMembers }) {
+    state.currentMembers = [...members];
+    state.possibleMembers = [...possibleMembers];
+  },
+  [ADD_MEMBER](state, member) {
+    state.currentMembers = [...state.currentMembers, member];
+    const index = state.possibleMembers.findIndex(user => user.id === member.id);
+    state.possibleMembers.splice(index, 1);
+  },
+  [REMOVE_MEMBER](state, { index, member }) {
+    state.currentMembers.splice(index, 1);
+    state.possibleMembers = [...state.possibleMembers, member];
   },
 };
--- app/javascript/store/modules/froggo_team/state.js
+++ app/javascript/store/modules/froggo_team/state.js
@@ -1,3 +1,5 @@
 export default {
   teamName: null,
+  currentMembers: null,
+  possibleMembers: null,
 };
