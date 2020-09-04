c627003c86e59055a2bf28424468df7f8b4695ae
pfarellano <pfarellano@uc.cl>
2020-09-04 14:54:13 -0400

    style(FroggoTeamsForm): change alert messages position

--- app/javascript/mixins/showMessageMixin.js
+++ app/javascript/mixins/showMessageMixin.js
@@ -3,7 +3,7 @@ export default {
     showMessage(message) {
       this.$toasted.show(message, {
         theme: 'toasted-primary',
-        position: 'top-center',
+        position: 'top-right',
         duration: 7000,
       });
     },
