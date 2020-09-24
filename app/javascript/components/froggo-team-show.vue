b07a49efec12f4e1817cfd7e331154dc19a8c125
pfarellano <pfarellano@uc.cl>
2020-09-24 15:56:48 -0300

    feat(FroggoTeams): update methods and vars in froggo team show vue

--- app/javascript/components/froggo-team-show.vue
+++ app/javascript/components/froggo-team-show.vue
@@ -2,49 +2,17 @@
   <div class="froggo-teams-show">
     <div class="froggo-teams-show__name">
       <div class="froggo-teams-show__name-container">
-        {{ teamName }}
-      </div>
-      <div
-        class="froggo-teams-show__white-button"
-        @click="editFroggoTeamName"
-      >
-        {{ $t("message.froggoTeams.editName") }}
-      </div>
-    </div>
-    <div
-      class="froggo-teams-show__edit-name-section"
-      v-if="editName"
-    >
-      <input
-        type="text"
-        v-model="newName"
-      >
-      <div
-        class="froggo-teams-show__gray-button"
-        @click="submitName()"
-      >
-        {{ $t("message.froggoTeams.saveName") }}
+        {{ froggoTeam.name }}
       </div>
     </div>
     <div class="froggo-teams-show__members">
       <div class="froggo-teams-show__member-title">
-        {{ $t("message.froggoTeams.addMember") }}
-      </div>
-      <div class="froggo-teams-show__dropdown">
-        <clickable-dropdown
-          :body-title="dropdownTitle"
-          :no-items-message="noUsersMessage"
-          :items="possibleMembers"
-          @item-clicked="onItemClicked"
-        />
-      </div>
-      <div class="froggo-teams-show__member-title">
         {{ $t("message.froggoTeams.members") }}
-        ( {{ membersCounter }} )
+        ( {{ froggoTeamMembers.length }} )
       </div>
       <div
         class="froggo-teams-show__member-container"
-        v-for="(user, index) in currentMembers"
+        v-for="(user) in froggoTeamMembers"
         :key="user.id"
       >
         <a
@@ -63,73 +31,45 @@
           <label class="switch">
             <input
               type="checkbox"
+              :checked="user.active"
               @click="changeMemberActivation(user)"
             >
             <span class="slider" />
           </label>
-          <div
-            class="froggo-teams-show__gray-button"
-            @click="removeMember(user, index)"
-          >
-            {{ $t("message.froggoTeams.deleteFromTeam") }}
-          </div>
         </div>
       </div>
     </div>
     <div class="froggo-teams-show__end-section">
       <div
-        class="froggo-teams-show__gray-button"
-        @click="deleteFroggoTeam"
+        class="froggo-teams-show__white-button"
+        @click="editTeam"
       >
-        {{ $t("message.froggoTeams.deleteTeam") }}
+        {{ $t("message.froggoTeams.editTeam") }}
       </div>
       <div
-        class="froggo-teams-show__gray-button"
-        @click="saveFroggoTeam"
+        class="froggo-teams-show__light-blue-button"
+        @click="saveMembersState"
       >
-        {{ $t("message.froggoTeams.saveTeam") }}
+        {{ $t("message.froggoTeams.saveChanges") }}
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
 import {
   UPDATE_FROGGO_TEAM,
-  DELETE_FROGGO_TEAM,
 } from '../store/action-types';
-import {
-  TEAM_NAME,
-  LOAD_MEMBERS,
-  ADD_MEMBER,
-  REMOVE_MEMBER,
-} from '../store/mutation-types';
-import ClickableDropdown from './clickable-dropdown';
 import showMessageMixin from '../mixins/showMessageMixin';
 
 export default {
   mixins: [showMessageMixin],
-  beforeMount() {
-    this.$store.commit(TEAM_NAME, this.froggoTeam.name);
-    this.$store.commit(LOAD_MEMBERS, { members: this.froggoTeamMembers,
-      possibleMembers: this.organizationMembers });
-  },
   data() {
     return {
-      dropdownTitle: 'Usuarios',
-      noUsersMessage: 'No se encontraron usuarios',
-      editName: false,
-      newName: '',
-      usersToAdd: [],
-      usersToRemove: [],
+      originalMembersStates: this.loadMembersStates(),
     };
   },
   props: {
-    userId: {
-      type: Number,
-      required: true,
-    },
     froggoTeam: {
       type: Object,
       required: true,
@@ -138,87 +78,41 @@ export default {
       type: Array,
       required: true,
     },
-    organizationMembers: {
-      type: Array,
-      required: true,
-    },
   },
   methods: {
-    editFroggoTeamName() {
-      this.editName = !this.editName;
+    changeMemberActivation(user) {
+      user.active = !user.active;
     },
-    submitName() {
+    saveMembersState() {
       this.$store.dispatch(UPDATE_FROGGO_TEAM, {
-        name: this.newName,
         id: this.froggoTeam.id,
+        changedMembersIds: this.getChangedMembersIds(),
       })
         .then(() => {
-          this.$store.commit(TEAM_NAME, this.newName);
+          this.showMessage(this.$i18n.t('message.froggoTeams.successfullySavedChanges'));
         })
         .catch(() => {
-          this.showMessage(this.$i18n.t('message.error.existentName'));
+          this.showMessage(this.$i18n.t('message.settings.error'));
         });
     },
-    onItemClicked({ item }) {
-      this.addMember(item);
+    getChangedMembersIds() {
+      const changedMembers = this.froggoTeamMembers
+        .filter(member => member.active !== this.originalMembersStates[member.id]);
+
+      return changedMembers.map(user => user.id);
     },
-    addMember(user) {
-      if (this.froggoTeamMembers.some(u => u.id === user.id)) {
-        const index = this.usersToRemove.findIndex(u => u.id === user.id);
-        this.usersToRemove.splice(index, 1);
-      } else {
-        this.usersToAdd = [...this.usersToAdd, user];
-      }
-      this.$store.commit(ADD_MEMBER, user);
+    editTeam() {
+      window.location.href = `/froggo_teams/${this.froggoTeam.id}/edit`;
     },
-    removeMember(user, userIndex) {
-      if (this.froggoTeamMembers.some(u => u.id === user.id)) {
-        this.usersToRemove = [...this.usersToRemove, user];
-      } else {
-        const index = this.usersToAdd.findIndex(u => u.id === user.id);
-        this.usersToAdd.splice(index, 1);
-      }
-      this.$store.commit(REMOVE_MEMBER, { index: userIndex, member: user });
+    loadMembersStates() {
+      const membersState = {};
+
+      this.froggoTeamMembers.forEach((team) => {
+        membersState[team.id] = team.active;
+      });
+
+      return membersState;
     },
-    changeMemberActivation(user) {
-      console.log('apretado');
-      console.log(user);
-    },
-    saveFroggoTeam() {
-      this.$store.dispatch(UPDATE_FROGGO_TEAM, {
-        newMembersIds: this.usersToAdd.map(user => user.id),
-        oldMembersIds: this.usersToRemove.map(user => user.id),
-        id: this.froggoTeam.id,
-      })
-        .then(() => {
-          this.showMessage(this.$i18n.t('message.froggoTeams.successfullySavedTeam'));
-          this.usersToAdd = [];
-          this.usersToRemove = [];
-        });
-    },
-    deleteFroggoTeam() {
-      this.$store.dispatch(DELETE_FROGGO_TEAM, {
-        id: this.froggoTeam.id,
-      })
-        .then(response => {
-          if (response) {
-            window.location.href = `/github_users/${this.userId}/froggo_teams`;
-          }
-        });
-    },
-  },
-  components: {
-    ClickableDropdown,
-  },
-  computed: {
-    membersCounter() {
-      return this.currentMembers.length;
-    },
-    ...mapState({
-      teamName: state => state.froggoTeam.teamName,
-      currentMembers: state => state.froggoTeam.currentMembers,
-      possibleMembers: state => state.froggoTeam.possibleMembers,
-    }),
   },
 };
 </script>
