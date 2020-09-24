45439946ef2e2fdd85cd6fe32bd7a297f1b3ad09
pfarellano <pfarellano@uc.cl>
2020-09-24 15:29:43 -0300

    feat(FroggoTeams): add get_froggo_team_members to controller show method

--- app/controllers/froggo_teams_controller.rb
+++ app/controllers/froggo_teams_controller.rb
@@ -15,8 +15,27 @@ class FroggoTeamsController < ApplicationController
 
   def show
     @froggo_team = FroggoTeam.find(params[:id])
-    @froggo_team_members = @froggo_team.github_users
-    @organization_members = @froggo_team.organization.members.all_except(@froggo_team_members)
+    @froggo_team_members = get_froggo_team_members(@froggo_team)
     @github_user = github_user
+  end
+
+  private
+
+  def get_froggo_team_members(froggo_team)
+    froggo_team.github_users.map do |member|
+      {
+        id: member.id,
+        avatar_url: member.avatar_url,
+        gh_id: member.gh_id,
+        login: member.login,
+        name: member.name,
+        active: get_member_activation(member, froggo_team)
+      }
+    end
+  end
+
+  def get_member_activation(member, froggo_team)
+    membership = FroggoTeamMembership.find_by(github_user: member, froggo_team: froggo_team)
+    membership.is_member_active
   end
 end
