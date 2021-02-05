95ff70ef1caa8b16cf7320c84ce41edbffbdd0aa
MariaJosefinaAlliende <moalliende@uc.cl>
2021-02-05 14:30:36 -0300

    feat(team-tags-table/TeamTags): Add componenent with user tags

---
+++ app/javascript/components/profile/team-tags.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="profile-team-tags__tags-container">
+    <div
+      v-for="tag in tags"
+      :class="`profile-team-tags__tag--${indexStyle}`"
+      :key="tag.id"
+    >
+      {{ tag.name }}
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  props: {
+    tags: {
+      type: Array,
+      default: () => { },
+    },
+    index: {
+      type: Number,
+      default: 0,
+    },
+  },
+  computed: {
+    indexStyle() {
+      const two = 2;
+
+      return this.index % two > 0 ? 'even' : 'odd';
+    },
+  },
+};
+</script>
+
