95b09fb04ecc6f79c0f842fea170fa3d4e268131
jdmizunuma <javiera.diaz.1@ug.uchile.cl>
2021-02-11 17:48:06 -0300

    feat(pull_request_reviewers): Add prs reviewer

--- app/javascript/components/open-pr.vue
+++ app/javascript/components/open-pr.vue
@@ -9,11 +9,21 @@
         Se ha detectado un PR tuyo con nombre:
       </h4>
       <h3>
-        #{{ pr }}
+        #{{ prTitle }}
       </h3>
+
+      <p
+        v-if="error"
+        class="profile-prs__error"
+      >
+        Error: {{ this.errors }}
+      </p>
+
       <v-select
         :options="users"
         label="login"
+        v-model="reviewer"
+        required
       >
         <template v-slot:no-options="{ search, searching }">
           <template v-if="searching">
@@ -39,7 +49,10 @@
         </template>
       </v-select>
       <div class="centered">
-        <button class="profile-prs__button">
+        <button
+          class="profile-prs__button"
+          @click="submit()"
+        >
           Asignar
         </button>
       </div>
@@ -50,6 +63,7 @@
 <script>
 import { mapState } from 'vuex';
 import colorFromScore from '../helpers/color-from-score';
+import pullRequestReviewersApi from '../api/pull_request_reviewers';
 
 export default {
   computed: {
@@ -60,15 +74,49 @@ export default {
     users() {
       return this.recommendations.all;
     },
+    prTitle() {
+      return this.pr.title;
+    },
+    prId() {
+      return this.pr.id;
+    },
   },
   props: {
     pr: {
-      type: String,
+      type: Object,
       required: true,
     },
   },
   methods: {
     colorFromScore,
+    submit() {
+      if (this.reviewer) {
+        this.addReviewer();
+      } else {
+        this.error = true;
+        this.errors = 'Debe seleccionar algún revisor';
+      }
+    },
+    addReviewer() {
+      const data = { reviewer: this.reviewer.login, pullRequestId: this.prId };
+      pullRequestReviewersApi.addReviewer(data)
+        .then(() => {
+          this.error = false;
+          this.reviewer = '';
+        }).catch(() => {
+          this.error = true;
+          this.errors = 'Hubo un problema ><';
+          this.reviewer = '';
+        });
+    },
+  },
+  data() {
+    return {
+      pullRequestId: this.prId,
+      reviewer: '',
+      error: false,
+      errors: '',
+    };
   },
 };
 </script>
