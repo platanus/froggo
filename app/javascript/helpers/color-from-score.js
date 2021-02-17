864b8fdbb3e21798246cbb5f02a4ef0dd8c2d399
MariaJosefinaAlliende <moalliende@uc.cl>
2021-02-17 20:55:13 -0300

    fix(team-tags-table/colorFromScore): Show red for -1 scores in table

--- app/javascript/helpers/color-from-score.js
+++ app/javascript/helpers/color-from-score.js
@@ -1,6 +1,6 @@
-export default function colorFromScore(score) {
+export default function colorFromScore(score, noColor = '') {
   /* eslint-disable no-magic-numbers*/
-  if (score === -1.0) return '';
+  if (score === -1.0) return noColor;
 
   if (score <= 0.25) return 'red';
   if (score <= 0.5) return 'light-red';
