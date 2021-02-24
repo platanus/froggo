61785794d965782828f859c1d33dc3841e573bb6
jdmizunuma <javiera.diaz.1@ug.uchile.cl>
2021-02-24 17:40:10 -0300

    test(pull-request-reviewers-controller): add test to pull_request_reviewers controller

---
+++ spec/controllers/api/v1/pull_request_reviewers_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::PullRequestReviewersController, type: :controller do
+  describe '# POST /api/pull_request_reviewer/add' do
+    it 'returns found status' do
+      post :add_reviewer
+      expect(response).to have_http_status(:found)
+    end
+  end
+end
