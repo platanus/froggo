c6d1834963932a496a666956d5fb2beb4c7ada16
Gabriel Lyon <glyon1@uc.cl>
2021-03-22 11:43:15 -0300

    test(process-membership-event): change doubles for correct data structure and test case for when user has not been created in froggo

--- spec/commands/github/process_membership_event_spec.rb
+++ spec/commands/github/process_membership_event_spec.rb
@@ -5,48 +5,80 @@ describe Github::ProcessMembershipEvent do
     described_class.for(event_payload: event_payload)
   end
 
-  let!(:user) { create(:github_user, gh_id: 1) }
-  let!(:organization) { create(:organization, gh_id: 234, default_team_id: 123) }
+  let(:organization_gh_id) { 234 }
+  let(:team_id) { 123 }
+  let!(:organization) { create(:organization, gh_id: organization_gh_id, default_team_id: team_id) }
+  let(:member_payload) do
+    {
+      login: "Codertocat",
+      id: user_gh_id,
+      avatar_url: "",
+      html_url: ""
+    }
+  end
   let(:event_payload) do
-    double(
+    RecursiveOpenStruct.new(
       action: action,
-      member: double(id: 1),
-      team: double(id: 123),
-      organization: double(id: 234)
+      member: member_payload,
+      team: { id: team_id },
+      organization: { id: organization_gh_id }
     )
   end
 
-  context 'when user is added to default team in Github' do
-    let!(:organization_membership) do
-      create(
-        :organization_membership,
-        github_user: user,
-        organization: organization,
-        is_member_of_default_team: false
-      )
-    end
+  context 'when user does not exist in Froggo' do
     let(:action) { 'added' }
+    let(:user_gh_id) { 345 }
 
-    it 'user is added to default team in local DB' do
-      expect { perform }.to change { organization_membership.reload.is_member_of_default_team }
-        .from(false).to(true)
+    it "doesn't add a new Organization Membership for him" do
+      expect { perform }.not_to(change { OrganizationMembership.count })
     end
   end
 
-  context 'when user is removed from default team in Github' do
-    let!(:organization_membership) do
-      create(
-        :organization_membership,
-        github_user: user,
-        organization: organization,
-        is_member_of_default_team: true
-      )
-    end
-    let(:action) { 'removed' }
+  context 'when user already exists in Froggo' do
+    let(:user_gh_id) { 1234 }
+    let!(:user) { create(:github_user, gh_id: user_gh_id) }
 
-    it 'user is removed from default team in local DB' do
-      expect { perform }.to change { organization_membership.reload.is_member_of_default_team }
-        .from(true).to(false)
+    context 'when user is added to new organization' do
+      let(:action) { 'added' }
+      let(:organization_gh_id) { 456 }
+
+      it 'creates new Organization Membership' do
+        expect { perform }.to change { OrganizationMembership.count }.by(1)
+      end
+    end
+
+    context 'when user is added to default team in Github' do
+      let!(:organization_membership) do
+        create(
+          :organization_membership,
+          github_user: user,
+          organization: organization,
+          is_member_of_default_team: false
+        )
+      end
+      let(:action) { 'added' }
+
+      it 'user is added to default team in local DB' do
+        expect { perform }.to change { organization_membership.reload.is_member_of_default_team }
+          .from(false).to(true)
+      end
+    end
+
+    context 'when user is removed from default team in Github' do
+      let!(:organization_membership) do
+        create(
+          :organization_membership,
+          github_user: user,
+          organization: organization,
+          is_member_of_default_team: true
+        )
+      end
+      let(:action) { 'removed' }
+
+      it 'user is removed from default team in local DB' do
+        expect { perform }.to change { organization_membership.reload.is_member_of_default_team }
+          .from(true).to(false)
+      end
     end
   end
 end
