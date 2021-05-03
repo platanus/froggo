6be61d3d7fee6ba6ce909dc8ba58c4ab4ebef1d7
Gabriel Lyon <glyon1@uc.cl>
2021-05-03 12:16:23 -0400

    feat(tailwind): update configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 /* eslint-disable no-undef */
 module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './app/**/*.html',
+      './app/**/*.vue',
+      './app/**/*.js',
+      './app/**/*.erb',
+    ],
+  },
   theme: {
     extend: {
       fontFamily: {
