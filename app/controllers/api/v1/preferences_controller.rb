81f491dc78ac196ff82bee10f4125e9776982070
Álvaro Tobar <ankaph@protonmail.com>
2021-07-13 10:17:35 -0400

    refactor(preferences_controller): add safe navigation for org_id and team_id

--- app/controllers/api/v1/preferences_controller.rb
+++ app/controllers/api/v1/preferences_controller.rb
@@ -14,8 +14,8 @@ class Api::V1::PreferencesController < Api::V1::BaseController
 
   def preference
     @preference ||= Preference.find_or_create_by(github_user_id: github_user.id) do |preference|
-      org_id = OrganizationMembership.find_by(github_user_id: github_user.id).organization_id
-      team_id = FroggoTeamMembership.find_by(github_user_id: github_user.id).froggo_team_id
+      org_id = OrganizationMembership.find_by(github_user_id: github_user.id)&.organization_id
+      team_id = FroggoTeamMembership.find_by(github_user_id: github_user.id)&.froggo_team_id
       preference.default_organization_id = org_id
       preference.default_team_id = team_id
       preference.default_time = 1
