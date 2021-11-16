783928f497063bfe4892938ca9a94efb44643df7
nicolasguridi <nguridi@uc.cl>
2021-11-16 10:53:55 -0300

    refactor(profile): adapt user description and tags to new design

--- app/javascript/components/profile/info.vue
+++ app/javascript/components/profile/info.vue
@@ -1,26 +1,32 @@
 <template>
   <div>
-    <div class="flex flex-row justify-between">
-      <p class="text-lg font-semibold mb-6">
-        Mis datos
-      </p>
-    </div>
-    <p class="text-sm font-semibold mb-1">
-      Acerca de mí
-    </p>
-    <p class="mb-4">
-      {{ githubUser.description }}
-    </p>
-    <p class="text-sm font-semibold mb-1">
-      Mis tags
-    </p>
-    <div class="flex flex-row items-center">
-      <div
-        v-for="tag in userTags"
-        :key="tag.id"
-        class="px-3 py-2 bg-red-500 rounded-full text-white mr-2"
-      >
-        {{ tag.name }}
+    <div>
+      <div class="flex flex-row justify-between">
+        <p class="text-lg font-semibold mb-6">
+          Mis datos
+        </p>
+        <froggo-button
+          v-if="$parent.canEdit"
+          class="inline-flex items-center px-2"
+          variant="blue"
+          :icon-file-name="editIcon"
+          :only-icon="true"
+          @click="toggleEdit"
+        />
+      </div>
+      <div class="mb-4">
+        <profile-description
+          :github-user="githubUser"
+          :github-session="githubSession"
+        />
+      </div>
+      <div>
+        <user-tags
+          :github-user="githubUser"
+          :github-session="githubSession"
+          :tags="tags"
+          :user-tags="userTags"
+        />
       </div>
     </div>
   </div>
@@ -28,7 +34,17 @@
 
 <script>
 
+import FroggoButton from '../shared/froggo-button.vue';
+import ProfileDescription from '../profile-description.vue';
+import UsersApi from '../../api/users';
+import UserTags from './user-tags.vue';
+
 export default {
+  components: {
+    FroggoButton,
+    ProfileDescription,
+    UserTags,
+  },
   props: {
     githubUser: {
       type: Object,
@@ -38,10 +54,71 @@ export default {
       type: Object,
       required: true,
     },
+    tags: {
+      type: Array,
+      required: true,
+    },
     userTags: {
       type: Array,
       required: true,
     },
+  },
+  computed: {
+    editIcon() {
+      return this.isEditing ? 'close-icon.svg' : 'edit-icon.svg';
+    },
+  },
+  methods: {
+    toggleEdit() {
+      this.isEditing = !this.isEditing;
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+      this.selectedTags = [];
+    },
+    onItemClicked(event) {
+      if (!this.myTags.map(this.getId).includes(event.item.id) &&
+          !this.selectedTags.map(this.getId).includes(event.item.id)) {
+        this.selectedTags.push(event.item);
+      }
+    },
+    getId(element) {
+      return element.id;
+    },
+    editTags(ids) {
+      UsersApi.updateUser(this.githubSession.user.id, {
+        tagIds: [... ids],
+      })
+        .then((res) => {
+          this.myTags = res.data.data.attributes.tags;
+        }).catch(() => {
+          this.error = true;
+        });
+    },
+    addTags() {
+      const ids = new Set([...this.myTags.map(this.getId), ... this.selectedTags.map(this.getId)]);
+      this.editTags(ids);
+      this.toggleModal();
+    },
+    eliminateTag(itemId) {
+      let ids = this.myTags.map(this.getId);
+      ids = ids.filter((item) => item !== itemId);
+      this.editTags(ids);
+    },
+    cancelTag(itemId) {
+      this.selectedTags = this.selectedTags.filter((item) => item.id !== itemId);
+    },
+  },
+  data() {
+    return {
+      isEditing: false,
+      dropdownTitle: 'Seleccionar tags',
+      noTagsMessage: 'No existen tags',
+      showModal: false,
+      selectedTags: [],
+      myTags: this.userTags,
+      error: false,
+    };
   },
 };
 </script>
